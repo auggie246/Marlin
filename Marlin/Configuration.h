6fa6f899b6e85b9772c5004389438b6bed7438ab
auggie246 <auggie.teo@gmail.com>
2020-08-19 22:35:44 +0800

    update thermistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -416,7 +416,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 5
+#define TEMP_SENSOR_0 66
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -447,9 +447,9 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   5
-#define HEATER_1_MINTEMP   5
-#define HEATER_2_MINTEMP   5
+#define HEATER_0_MINTEMP   21
+#define HEATER_1_MINTEMP   21
+#define HEATER_2_MINTEMP   21
 #define HEATER_3_MINTEMP   5
 #define HEATER_4_MINTEMP   5
 #define HEATER_5_MINTEMP   5
@@ -460,9 +460,9 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 275
-#define HEATER_1_MAXTEMP 275
-#define HEATER_2_MAXTEMP 275
+#define HEATER_0_MAXTEMP 280
+#define HEATER_1_MAXTEMP 280
+#define HEATER_2_MAXTEMP 280
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
 #define HEATER_5_MAXTEMP 275
@@ -505,9 +505,9 @@
   //#define DEFAULT_Kd 440
 
   // D-Bot Auggie's
-  #define DEFAULT_Kp 14.38
-  #define DEFAULT_Ki 0.68
-  #define DEFAULT_Kd 76.27
+  #define DEFAULT_Kp 14.0
+  #define DEFAULT_Ki 0.5
+  #define DEFAULT_Kd 125.0
 
 #endif // PIDTEMP
 
